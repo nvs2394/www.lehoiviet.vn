7ea50223737ff6d314e98fb75f3a1fe22cffdce9
Nguyễn Văn Sơn <son.nguyenvan@elinext.com>
2017-04-27 18:15:37 +0700

    fix ssl

--- app/application.js
+++ app/application.js
@@ -98,8 +98,8 @@ app.config(['$routeProvider', function($routeProvider) {
 }]);
 
 app.constant("ENV", {
-    apiUrl: "http://api.lehoiviet.vn",
-    gateWay: "http://api.lehoiviet.vn"
+    apiUrl: "https://api.lehoiviet.vn",
+    gateWay: "https://api.lehoiviet.vn"
 
     // apiUrl: "http://127.0.0.1:3000",
     // gateWay: "http://127.0.0.1:3000"
